9043f856ee022a8548e78f8510b6f5acf908eab4
Adrian Perez <perezA@appacademy.us>
2015-01-14 14:01:16 -0800

    sdved

--- public_html/script.js
+++ public_html/script.js
@@ -10,15 +10,18 @@ $("document").ready(function(){
     $('p:last').css({"background-color":"red", "color": "white"});  
    
    
-    $('oneButton').bind('click', alertButtonClick);
+   // $('oneButton').bind('click', alertButtonClick);
     
-    $('logo').bind('mouseover', mouseOverme).bind('mouseout', mouseOutMe);
+   // $('logo').bind('mouseover', mouseOverme).bind('mouseout', mouseOutMe);
     
-    $('#repLaceWTetxt').bind('click', repLaceWText);
+   // $('#repLaceWTetxt').bind('click', repLaceWText);
     
     $('#addAPara').bind('click', addAPara);
     
     $('#removeAPara').bind('click', removeAPara);
+    
+    $('#replaceWHtml').bind('click', replaceWHtml);
+$('#replaceWText').bind('click', replaceWText);
 });
 
 function removeAPara(){
@@ -29,6 +32,12 @@ function addAPara(){
     $('randPara').append('p2ADDED/p2');
 }
 
+function replaceWHtml(){
+    $('#h3Tag').html('<h6>I\'m an h6</h6>');
+}
+
+
 function replaceWText(){
     $('#replaceWText').text('Replace');
+    $('#h3Tag').text('<h6>I\'m an h6</h6>');
 }
